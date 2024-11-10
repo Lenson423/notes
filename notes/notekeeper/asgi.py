2b757ecd680505a98864a1366b42495d6ce2e9ed
Eugene Bohdan <lenson.eab@gmail.com>
2024-11-10 16:25:21 +0300

    little fix

--- notes/notekeeper/asgi.py
+++ notes/notekeeper/asgi.py
@@ -1,16 +1,22 @@
 import os
-import chat.routing
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+
+from chat import consumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
+ws_urlpatterns = [
+    path("ws/<str:room_name>/", consumer.ChatConsumer.as_asgi()),
+]
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            chat.routing.ws_urlpatterns
+            ws_urlpatterns
         )
     )
 })
